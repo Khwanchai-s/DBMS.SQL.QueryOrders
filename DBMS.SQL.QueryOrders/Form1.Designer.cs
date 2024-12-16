71fece4fb230548d8e79f02edb6b5171d6a91e28
Khwanchai-s <khwanchai.s@gmail.com>
2024-12-16 15:52:09 +0700

    Add project files.

---
+++ DBMS.SQL.QueryOrders/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DBMS.SQL.QueryOrders
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvOrders = new DataGridView();
+            dgvDetails = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvOrders
+            // 
+            dgvOrders.AllowUserToAddRows = false;
+            dgvOrders.AllowUserToDeleteRows = false;
+            dgvOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrders.Location = new Point(31, 51);
+            dgvOrders.Name = "dgvOrders";
+            dgvOrders.ReadOnly = true;
+            dgvOrders.RowHeadersWidth = 51;
+            dgvOrders.Size = new Size(1003, 329);
+            dgvOrders.TabIndex = 0;
+            // 
+            // dgvDetails
+            // 
+            dgvDetails.AllowUserToAddRows = false;
+            dgvDetails.AllowUserToDeleteRows = false;
+            dgvDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDetails.Location = new Point(33, 426);
+            dgvDetails.Name = "dgvDetails";
+            dgvDetails.ReadOnly = true;
+            dgvDetails.RowHeadersWidth = 51;
+            dgvDetails.Size = new Size(1001, 188);
+            dgvDetails.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 28);
+            label1.TabIndex = 2;
+            label1.Text = "ใบสั่งซื้อ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 395);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 28);
+            label2.TabIndex = 2;
+            label2.Text = "รายการสินค้า";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(847, 317);
+            label3.Name = "label3";
+            label3.Size = new Size(158, 41);
+            label3.TabIndex = 3;
+            label3.Text = "dgvOrders";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F);
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(847, 560);
+            label4.Name = "label4";
+            label4.Size = new Size(157, 41);
+            label4.TabIndex = 3;
+            label4.Text = "dgvDetails";
+            label4.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1072, 657);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvDetails);
+            Controls.Add(dgvOrders);
+            Font = new Font("Segoe UI", 12F);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "DBMS.SQL.QueryOrders";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvOrders).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvDetails).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvOrders;
+        private DataGridView dgvDetails;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ DBMS.SQL.QueryOrders/Form1.cs
@@ -0,0 +1,58 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DBMS.SQL.QueryOrders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //ประกาศตัวแปร connect
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        private void connectDB()   //ใช้เปิดการเชื่อมต่อฐานข้อมูล
+        {
+            string server = @".\sqlexpress"; // ไปcopy ชื่อ server มาจาก SqlServer
+            string db = "northwind";
+            string strCon = string.Format(@"Data Source={0};Initial Catalog={1};"
+                            + "Integrated Security=True;Encrypt=False", server, db);
+            conn = new SqlConnection(strCon);
+            conn.Open();
+        }
+        private void disconnectDB()
+        {
+            conn.Close();
+        }
+
+        private void showdata(string sql, DataGridView dgv)  //ส่ง sql และ dgv ที่แสดงผล
+        {
+            da = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dgv.DataSource = ds.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectDB();
+            //ไปเอา SQL ที่ทดสอบแล้วมาใส่ตรง sqlQuery
+            string sqlQuery = "Select o.OrderID, Format(o.OrderDate,'dd-MMM-yyyy') as Order_Date , "
+                             + " Format(o.ShippedDate,'dd-MMM-yyyy') as Shipped_Date, "
+                             + " sh.CompanyName, TitleOfCourtesy+firstname + ' ' + LastName EmployeeName, "
+                             + " c.CompanyName, c.Phone," 
+                             + " Convert(decimal(10,2),sum(od.Quantity * od.UnitPrice *(1-od.Discount))) TotalCash"
+                             + " from orders o join [Order Details] od on o.OrderID = od.OrderID"
+                             + " join Shippers sh on sh.ShipperID = o.ShipVia"
+                             + " join Employees e on o.EmployeeID = e.EmployeeID"
+                             + " join Customers c on o.CustomerID = c.CustomerID"
+                             + " group by o.OrderID, o.OrderDate, o.ShippedDate, sh.CompanyName, "
+                             + " TitleOfCourtesy+firstname + ' ' + LastName, c.CompanyName, c.Phone"; 
+            showdata(sqlQuery, dgvOrders);
+
+        }
+    }
+}
